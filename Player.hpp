bd94cc4660226a4455ba5f541b3b8d0e5a4389e2
aroze666 <aroze666@gmail.com>
2023-10-01 15:25:54 -0700

    formatting

--- Player.hpp
+++ Player.hpp
@@ -6,30 +6,30 @@
 #include <list>
 class Player : public sf::Drawable
 {
-	public:
-		Player();
-		void Update();
-		void Event();
-		void SetMovementRange(sf::Vector2f range);
-		void SetHitboxSize(sf::Vector2f size);
-		void SetBulletSize(sf::Vector2f size);
-		void SetPosition(sf::Vector2f position);
-		void SetVelocity(sf::Vector2f velocity);
-		sf::Vector2f GetVelocity(){return velocity;}
-		sf::RectangleShape GetHitbox();
-	private:
-		virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
-		{
-			target.draw(hitbox, states);
-			for(auto it=bullet_list.begin(); it!=bullet_list.end(); it++)
-				target.draw(*it, states);
-		}
-		sf::RectangleShape hitbox;
-		sf::Vector2f bullet_size;
-		std::list<sf::RectangleShape> bullet_list;
-		sf::Vector2f velocity;
-		sf::Vector2f movement_range;
+    public:
+        Player();
+        void Update();
+        void Event();
+        void SetMovementRange(sf::Vector2f range);
+        void SetHitboxSize(sf::Vector2f size);
+        void SetBulletSize(sf::Vector2f size);
+        void SetPosition(sf::Vector2f position);
+        void SetVelocity(sf::Vector2f velocity);
+        sf::Vector2f GetVelocity(){return velocity;}
+        sf::RectangleShape GetHitbox();
+    private:
+        virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
+        {
+            target.draw(hitbox, states);
+            for(auto it=bullet_list.begin(); it!=bullet_list.end(); it++)
+                target.draw(*it, states);
+        }
+        sf::RectangleShape hitbox;
+        sf::Vector2f bullet_size;
+        std::list<sf::RectangleShape> bullet_list;
+        sf::Vector2f velocity;
+        sf::Vector2f movement_range;
 };
 
 
-#endif
+#endif    
