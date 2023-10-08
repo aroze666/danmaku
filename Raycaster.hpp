a18138611a50f0617d526fa0fa3c72a4dec8b871
aroze666 <aroze666@gmail.com>
2023-10-07 19:03:49 -0700

    added 90 degree pov (raycasting)

--- Raycaster.hpp
+++ Raycaster.hpp
@@ -10,71 +10,93 @@ class RayCaster : public sf::Drawable
     public:
     RayCaster()
     {
-        ray.setFillColor(sf::Color::Magenta);
-        // ray.setSize({2,10});
+        number_of_rays = 90;
+        for(int i = 0; i<number_of_rays;i++)
+        {
+            sf::RectangleShape new_ray;
+            new_ray.setFillColor(sf::Color::Magenta);
+            rays.push_back(new_ray);
+        }
     }
-    void Update(sf::Vector2f player_position, sf::Vector2f aim_direction, Scene &scene)
+    void Update(sf::Vector2f position, sf::Vector2f aim_direction, Scene &scene)
     {
-        sf::Vector2f d = {(float)std::sqrt(1+std::pow(aim_direction.y/aim_direction.x,2)), (float)std::sqrt(1+std::pow(aim_direction.x/aim_direction.y,2))};
-        sf::Vector2f grid_start = player_position;
-        grid_start.x = grid_start.x/64.f;
-        grid_start.y = grid_start.y/64.f;
-        sf::Vector2i grid_map;
-        sf::Vector2i step;
-        sf::Vector2f distance;
-        grid_map.x = grid_start.x;
-        grid_map.y = grid_start.y;
-        if(aim_direction.x < 0)
+        sf::Vector2f direction = aim_direction;
+        float angle = std::atan(direction.y/direction.x);
+        angle -= 3.1415026/4;
+        if(direction.x < 0)
         {
-            step.x = -1;
-            distance.x = 64*(grid_start.x - (float)grid_map.x)*d.x;
-        }else if(aim_direction.x > 0)
-        {
-            step.x = 1;
-            distance.x = 64*((float)(grid_map.x+1) - grid_start.x)*d.x;
+            angle += 3.1415926;
         }
-        if(aim_direction.y < 0)
+        direction.x = std::cos(angle);
+        direction.y = std::sin(angle);
+        for(int i = 0; i<rays.size(); i++)
         {
-            step.y = -1;
-            distance.y = 64*(grid_start.y - (float)grid_map.y)*d.y;
-
-        }else if(aim_direction.y >0)
-        {
-            step.y = 1;
-            distance.y = 64*((float)(grid_map.y+1) - grid_start.y)* d.y;
-        }
-        while(true)
-        {
-            if(scene.grids[grid_map.y][grid_map.x].wall)
-                break;
-            if(distance.x > distance.y)
+            sf::Vector2f d = {(float)std::sqrt(1+std::pow(direction.y/direction.x,2)), (float)std::sqrt(1+std::pow(direction.x/direction.y,2))};
+            sf::Vector2f grid_start = position;
+            grid_start.x = grid_start.x/64.f;
+            grid_start.y = grid_start.y/64.f;
+            sf::Vector2i grid_map;
+            sf::Vector2i step;
+            sf::Vector2f distance;
+            grid_map.x = grid_start.x;
+            grid_map.y = grid_start.y;
+            if(direction.x < 0)
             {
-                grid_map.y += step.y;
-                if(!scene.grids[grid_map.y][grid_map.x].wall)
-                    distance.y += 64*d.y;
-            }else{
-                grid_map.x += step.x;
-                if(!scene.grids[grid_map.y][grid_map.x].wall)
-                    distance.x += 64*d.x;
+                step.x = -1;
+                distance.x = 64*(grid_start.x - (float)grid_map.x)*d.x;
+            }else if(direction.x > 0)
+            {
+                step.x = 1;
+                distance.x = 64*((float)(grid_map.x+1) - grid_start.x)*d.x;
             }
+            if(direction.y < 0)
+            {
+                step.y = -1;
+                distance.y = 64*(grid_start.y - (float)grid_map.y)*d.y;
+
+            }else if(direction.y >0)
+            {
+                step.y = 1;
+                distance.y = 64*((float)(grid_map.y+1) - grid_start.y)* d.y;
+            }
+            while(true)
+            {
+                if(scene.grids[grid_map.y][grid_map.x].wall)
+                    break;
+                if(distance.x > distance.y)
+                {
+                    grid_map.y += step.y;
+                    if(!scene.grids[grid_map.y][grid_map.x].wall)
+                        distance.y += 64*d.y;
+                }else{
+                    grid_map.x += step.x;
+                    if(!scene.grids[grid_map.y][grid_map.x].wall)
+                        distance.x += 64*d.x;
+                }
+            }
+
+            if(distance.x < distance.y)
+                rays[i].setSize({distance.x, 1});
+            else
+                rays[i].setSize({distance.y, 1});
+            rays[i].setPosition(position);
+            if(direction.x < 0)
+                rays[i].setRotation(180+std::atan(direction.y/direction.x)*180/3.1415926);
+            else
+                rays[i].setRotation(std::atan(direction.y/direction.x)*180/3.1415926);
+            angle += 3.1415026/180;
+            direction.x = std::cos(angle);
+            direction.y = std::sin(angle);
         }
-        float angle = 0;
-        if(distance.x < distance.y)
-            ray.setSize({distance.x, 1});
-        else
-            ray.setSize({distance.y, 1});
-        ray.setPosition(player_position);
-        angle = std::atan(aim_direction.y/aim_direction.x)*180/3.1415926;
-        if(aim_direction.x < 0)
-            angle+=180;
-        ray.setRotation(angle);
     }
     private:
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const
     {
-        target.draw(ray, states);
+        for(int i = 0; i<rays.size(); i++)
+            target.draw(rays[i], states);
     }
-    sf::RectangleShape ray;
+    int number_of_rays;
+    std::vector<sf::RectangleShape> rays;
 };
 
 #endif
