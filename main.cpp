35d4021ec1fabaea48dc63580af1885d497f92e7
aroze666 <aroze666@gmail.com>
2023-10-10 16:14:36 -0700

    format

--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
-#include <SFML/Graphics.hpp>
 #include "Game.hpp"
-int main (int argc, char *argv[]) {
-    sf::RenderWindow window(sf::VideoMode(1024,1024), "SFML works!");
-    window.setFramerateLimit(60);
-    // window.setMouseCursorVisible(false);
-    Game game(window);
-    while(window.isOpen())
-    {
-	sf::Event event;
-	while(window.pollEvent(event))
-	{
-	    if(event.type == sf::Event::Closed)
-		window.close();
-	}
-	game.GeneralEvent();
-	game.CollisionEvent();
-	game.Update();
-	window.clear(sf::Color(128,128,128,255));
-	window.draw(game);
-	window.display();
+#include <SFML/Graphics.hpp>
+int main(int argc, char *argv[]) {
+  sf::RenderWindow window(sf::VideoMode(1024, 1024), "SFML works!");
+  window.setFramerateLimit(60);
+  // window.setMouseCursorVisible(false);
+  Game game(window);
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
     }
-    return 0;
+    game.GeneralEvent();
+    game.CollisionEvent();
+    game.Update();
+    window.clear(sf::Color(128, 128, 128, 255));
+    window.draw(game);
+    window.display();
+  }
+  return 0;
 }
